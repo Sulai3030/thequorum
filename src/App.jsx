58eec1e624d85e4320f7f6f113e7a5230c02b2ab
sssulai@gmail.com <you@example.com>
2026-01-16 16:59:31 -0500

    added who and news views

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+import Nav from "./components/nav.jsx";
 import './App.css'
-
+import News from "./components/News.jsx";
+import Who from "./components/Who.jsx";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <div className="card">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <Who />
+      <News />
     </>
   );
 }
